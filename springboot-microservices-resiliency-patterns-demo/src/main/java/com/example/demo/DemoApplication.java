cbed2fd12ecab4546207fa9cc106ac3f54d18806
“Sajal <sajal.chakraborty@gmail.com>
2026-01-03 01:08:31 +0530

    test

--- springboot-microservices-resiliency-patterns-demo/src/main/java/com/example/demo/DemoApplication.java
+++ springboot-microservices-resiliency-patterns-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 
+
         System.out.println("hello from resiliency module");
 		SpringApplication.run(DemoApplication.class, args);
 	}
